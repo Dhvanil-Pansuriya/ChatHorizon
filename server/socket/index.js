7a0d7a8e986077471471dec9562d95003d0994c4
Dhvanil Pansuriya <dhvanilpansuriya2005@gmail.com>
2024-10-04 09:45:00 +0530

    changes

--- server/socket/index.js
+++ server/socket/index.js
@@ -3,6 +3,7 @@ import { Server } from "socket.io";
 import http from "http";
 import dotenv from "dotenv";
 import getUserDetailFromToken from "../helper/getUserDetailFromToken.js";
+import { User } from "../models/user.model.js";
 
 const app = express();
 
@@ -19,7 +20,6 @@ const io = new Server(server, {
   },
 });
 
-
 const onlineUser = new Set();
 
 io.on("connection", async (socket) => {
@@ -31,9 +31,23 @@ io.on("connection", async (socket) => {
   // console.log("User : ", user);
 
   socket.join(user?._id);
-  onlineUser.add(user?._id);
+  onlineUser.add(user?._id?.toString());
 
   io.emit("onlineUser", Array.from(onlineUser));
+
+  socket.on("message-page", async (userId) => {
+    const userDetail = await User.findById(userId).select("-password");
+
+    const payload = {
+      _id: userDetail?._id,
+      name: userDetail?.name,
+      email: userDetail?.email,
+      profile_pic: userDetail?.profile_pic,
+      online: onlineUser.has(userId),
+    };
+
+    socket.emit("message-user", payload);
+  });
 
   /**Socket Disconnection */
   socket.on("disconnect", () => {
